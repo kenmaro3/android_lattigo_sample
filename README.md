e61805c735f5afed92999acb6eb45c67b43c0c62
kenmaro3 <miha.ken.19@gmail.com>
2023-10-25 17:48:21 +0900

    [ADD] readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#### tutorial screen record
+
+https://drive.google.com/file/d/15Fb74q_wFkM7MXkSQbZROtNj48Om9ay9/view?usp=sharing
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation(files("/Users/kmihara/Documents/test/simple/simple.aar"))
+    implementation(files("/Users/kmihara/Documents/lattigo_wasm_ios_android/simple/simple.aar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
